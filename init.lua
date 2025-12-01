4c993246160c2de602e975bb5686ce093e94cf6e
hanbali <x@hanbali@com>
2025-12-01 16:04:52 -0600

    removed mason

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
 require('config.options')
 require('config.keybinds')
 require('config.lazy')
-require("lazy").setup({
-  spec = {
-    { import = "plugins" },  -- This line loads ALL lua/plugins/*.lua
-  }
-})
-
+require("lazy").setup({})
