229bcea4c920d4b47923e70cda4bf82ac9790b31
hanbali <x@hanbali@com>
2025-11-30 19:45:37 -0600

    plugin to make man pages easy to read

---
+++ after/ftplugin/man.lua
@@ -0,0 +1,7 @@
+local set = vim.opt_local
+set.number = true           -- Line numbers for jumping
+set.relativenumber = false  -- Reading mode (absolute only)
+set.wrap = true             -- Wrap long lines
+set.linebreak = true        -- Break at spaces (not mid-word)
+set.conceallevel = 0        -- Show all characters
+
