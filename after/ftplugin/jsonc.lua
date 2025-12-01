c5b6e652055a843dfac800ebce0891700fe67653
hanbali <x@hanbali@com>
2025-11-30 19:47:26 -0600

    plugin for json editing

---
+++ after/ftplugin/jsonc.lua
@@ -0,0 +1,10 @@
+local set = vim.opt_local              -- Current JSON buffer only
+
+set.shiftwidth = 2                     -- 2 spaces for >>/<< indent
+set.tabstop = 2                        -- Tab key = 2 spaces  
+set.softtabstop = 2                    -- Backspace deletes 2 spaces
+set.expandtab = true                   -- Tab → spaces (never real tabs)
+
+set.number = true                      -- Line numbers
+set.relativenumber = true              -- Relative numbers (j/k motions)
+
