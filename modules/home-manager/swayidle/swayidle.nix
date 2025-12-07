c354b7a5fcbd58b0432e6d47237685d7d2cbbdaa
hanbali <x@hanbalix.com>
2025-12-07 03:10:02 -0600

    fixed swaylock

--- modules/home-manager/swayidle/swayidle.nix
+++ modules/home-manager/swayidle/swayidle.nix
@@ -1,51 +1,35 @@
 { config, pkgs, settings, ... }: {
 
-
-  services.swayidle =
-let
-  lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
-  display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
-in
-{
-  enable = true;
-  timeouts = [
-    {
-      timeout = 295; # 4min 55s in seconds
-      command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
-    }
-    {
-      timeout = 300; #5min
-      command = lock;
-    }
-    {
-      timeout = 310; 
-      command = display "off";
-      resumeCommand = display "on";
-    }
-    {
-      timeout = 320;
-      command = "${pkgs.systemd}/bin/systemctl suspend";
-    }
-  ];
-  events = {
-    {
-      event = "before-sleep";
-      # adding duplicated entries for the same event may not work
-      command = (display "off") + "; " + lock;
-    }
-    {
-      event = "after-resume";
-      command = display "on";
-    }
-    {
-      event = "lock";
-      command = (display "off") + "; " + lock;
-    }
-    {
-      event = "unlock";
-      command = display "on";
-    }
-        };
-};
-
+  services.swayidle = let
+    lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
+    display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+  in {
+    enable = true;
+    timeouts = [
+      {
+        timeout = 295; # 4min 55s
+        command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+      }
+      {
+        timeout = 300; # 5min
+        command = lock;
+      }
+      {
+        timeout = 310; 
+        command = display "off";
+        resumeCommand = display "on";
+      }
+      {
+        timeout = 320;
+        command = "${pkgs.systemd}/bin/systemctl suspend";
+      }
+    ];
+    events = {
+      "before-sleep" = "${display "off"}; ${lock}";
+      "after-resume" = display "on";
+      "lock" = "${display "off"}; ${lock}";
+      "unlock" = display "on";
+    };
+  };
 }
+
