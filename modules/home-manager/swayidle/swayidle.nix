6529916ec903ee5e06152c0b196d8f82beac5e05
hanbali <x@hanbalix.com>
2025-12-07 03:08:01 -0600

    fixed swaylock

--- modules/home-manager/swayidle/swayidle.nix
+++ modules/home-manager/swayidle/swayidle.nix
@@ -3,35 +3,31 @@
 
   services.swayidle =
 let
-  # Lock command
   lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
-  # TODO: modify "display" function based on your window manager
-  # Sway
-  # Niri
   display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
 in
 {
   enable = true;
   timeouts = [
     {
-      timeout = 15; # in seconds
+      timeout = 295; # 4min 55s in seconds
       command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
     }
     {
-      timeout = 20;
+      timeout = 300; #5min
       command = lock;
     }
     {
-      timeout = 25;
+      timeout = 310; 
       command = display "off";
       resumeCommand = display "on";
     }
     {
-      timeout = 30;
+      timeout = 320;
       command = "${pkgs.systemd}/bin/systemctl suspend";
     }
   ];
-  events = [
+  events = {
     {
       event = "before-sleep";
       # adding duplicated entries for the same event may not work
@@ -49,7 +45,7 @@ in
       event = "unlock";
       command = display "on";
     }
-  ];
+        };
 };
 
 }
